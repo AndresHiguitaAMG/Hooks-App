bb911b2e5c53de190a4e4f64fc599910267111bd
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-23 19:46:05 -0500

    concepts about the useEffect

---
+++ src/02-useEffect/SimpleForm.jsx
@@ -0,0 +1,53 @@
+import { useEffect, useState } from "react";
+
+const SimpleForm = () => {
+    const [ formState, setFormState ] = useState(
+        {
+            username: "Andres",
+            email: "example@google.com",
+        }
+    );
+    
+    const { username, email } = formState;
+
+    useEffect(() => {
+        console.log('Use')
+    }, []);
+
+    useEffect(() => {
+        console.log('Usef')
+    }, [formState]);
+
+    const handleOnChange = e => {
+        setFormState({
+            ...formState,
+            [e.target.name]: e.target.value
+        });
+    };
+
+  return (
+    <>
+        <h1>Formulario Simple</h1>
+        <hr />
+        <input 
+        type={'text'}
+        className="form-control"
+        placeholder="username"
+        name={'username'}
+        value={username}
+        onChange={handleOnChange}
+        />
+        
+        <input 
+        type={'email'}
+        className="form-control mt-2"
+        placeholder="example@google.com"
+        name={'email'}
+        value={email}
+        onChange={handleOnChange}
+        />
+    </>
+  )
+}
+
+export default SimpleForm;
