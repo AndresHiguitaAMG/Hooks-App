3fa9f479555bc4b18fa73302ef0690a25955d4d5
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-26 16:53:39 -0500

    precautions with the useEffect

--- src/02-useEffect/Message.jsx
+++ src/02-useEffect/Message.jsx
@@ -1,18 +1,23 @@
-import { useEffect } from "react";
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 const Message = () => {
-    useEffect(() => {
-      console.log("Message Mounted")
-    
-      return () => {
-        console.log("Message Unmounted")
+   const [ coords, setCoords ] = useState({ x: 0, y: 0 });
 
+    useEffect(() => {
+      const onMouseMove = ({ x, y }) => {
+        setCoords({ x, y });
       }
-    }, [])
+      window.addEventListener("mousemove", onMouseMove);
+      return () => {
+        window.removeEventListener("mousemove", onMouseMove);
+      }
+    }, []);
     
   return (
     <>
-        <h3>Usuario ya existe</h3>
+        <h3>Usuario ya existe</h3>;
+        {JSON.stringify(coords)};
     </>
   )
 }
