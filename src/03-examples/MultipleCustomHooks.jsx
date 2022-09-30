7f079d9566f997dcc3cb3b5b3e89b3b94f9adae0
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-09-29 23:58:50 -0500

    implementing the useFetch

---
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MultipleCustomHooks = () => {
+  return (
+    <>
+      <h1>Breaking Bad Quotes</h1>
+      <hr />
+    </>
+  );
+};
+
+export default MultipleCustomHooks;
