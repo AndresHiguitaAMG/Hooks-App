af4289c28a86d987f255860e09d97fe6b08c5a15
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-10-01 00:26:13 -0500

    breaking bad API

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,6 +1,8 @@
-import React from "react";
+import useFetch from '../hooks/useFetch';
 
 const MultipleCustomHooks = () => {
+  const {data, isLoading, hasError} = useFetch("https://www.breakingbadapi.com/api/quotes/1");
+
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,35 @@
-function useFetch() {
+import { useEffect, useState } from "react";
+
+function useFetch(url) {
+  const [state, setState] = useState({
+    data: null,
+    isLoading: true,
+    hasError: null,
+  });
+
+  const getFetch = async () => {
+    setState({
+      ...state,
+      isLoading: true,
+    });
+    const resp = await fetch(url);
+    const data = await resp.json();
+    setState({
+      data,
+      isLoading: false,
+      hasError: null,
+    });
+  };
+
+  useEffect(() => {
+    getFetch();
+  }, [url]);
+
   return {
-    
-  }
+    data: state.data,
+    isLoading: state.isLoading,
+    hasError: state.hasError,
+  };
 }
 
-export default useFetch
+export default useFetch;
