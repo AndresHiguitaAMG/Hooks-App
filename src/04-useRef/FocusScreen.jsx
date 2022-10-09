659aa01b02b256e77159cd8fdf97dbf38710c27c
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-10-08 23:38:16 -0500

    implementation of useRef

---
+++ src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,14 @@
+export const FocusScreen = () => {
+  return (
+    <>
+      <h1>Focus Screen</h1>
+      <hr />
+
+      <input
+        className="form-control"
+        type="text"
+        placeholder="Ingrese su nombre"
+      />
+    </>
+  );
+};
