87d1f9a0a6d08afde77dd1135f39d0e9c4d73e74
Edwin Andres Higuita Chaves <andreshiguitaamg@gmail.com>
2022-12-01 20:27:09 -0500

    implementation of useCallback

---
+++ src/06-memos/ShowIncrement.jsx
@@ -0,0 +1,13 @@
+export const ShowIncrement = ({increment}) => {
+    console.log("Me volví a generar :(")
+
+  return (
+    <button
+        className="btn btn-primary"
+        onClick={() => {
+            increment();
+        }}
+    >
+        Incrementar</button>
+  )
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { CallbackHook } from "./06-memos/CallbackHook";
 import { MemoHook } from "./06-memos/MemoHook";
 import { Memorize } from "./06-memos/Memorize";
 // import SimpleForm from "./02-useEffect/SimpleForm";
@@ -14,6 +15,6 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   // <React.StrictMode>
-  <MemoHook />
+  <CallbackHook />
   // </React.StrictMode>
 );
