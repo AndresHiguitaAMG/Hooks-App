177a4081d4114b62bb81fed2cab990cd89127fd9
AndresHiguitaAMG <andreshiguitaamg@gmail.com>
2023-01-16 23:55:40 -0500

    form

--- src/08-useReducer/TodoApp.jsx
+++ src/08-useReducer/TodoApp.jsx
@@ -21,11 +21,30 @@ export const TodoApp = () => {
     <>
         <h1>TodoApp</h1>
         <hr />
-        <ul>
-            <li>Item 1</li>
-            <li>Item 2</li>
-            <li>Item 3</li>
-        </ul>
+        <div className="row">
+          <div className="col-7">
+            <ul>
+              <li>Item 1</li>
+              <li>Item 2</li>
+              <li>Item 3</li>
+            </ul>
+          </div>
+          <div className="col-5">
+            <h4>Agregar Todo</h4>
+            <hr />
+            <form>
+              <input 
+              type="text"
+              placeholder="¿qué hay que hacer?"
+              className="form-control"
+              />
+              <button 
+              type="submit"
+              className="btn btn-outline-primary mt-1"
+              >Agregar</button>
+            </form>
+          </div>
+        </div>
     </>
   )
 };
